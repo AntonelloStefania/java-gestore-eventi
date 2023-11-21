6a0a9a89ee617b7b12f722e797d2ccaa270aedca
AntonelloStefania <antonelloste94@gmail.com>
2023-11-21 10:57:12 +0100

    milestone 1

---
+++ src/org/java/Main.java
@@ -0,0 +1,20 @@
+package org.java;
+
+import java.time.LocalDate;
+
+
+import org.java.pojo.Evento;
+
+public class Main {
+	public static void main(String[] args)  {
+		Evento event;
+		
+		try {
+			event = new Evento("concerto",LocalDate.parse("2023-12-28"), 1 );					
+			System.out.println(event);
+		} catch (Exception e) {
+			System.out.println("Errore: " + e.getMessage());
+		}
+		
+	}
+}
